2f73fdf315a42fdb7f6bc4cbdc045a80e0e868ae
tristan-robinette <tristan-robinette@hotmail.ca>
2024-09-17 22:38:08 +0100

    init models

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from app.models import Conversation
+from app.models import Message
+
+admin.site.register(Conversation)
+admin.site.register(Message)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,39 @@
-# Create your models here.
+from django.db import models
+
+from goglobal.users.models import User
+
+
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    language = models.CharField(max_length=50, default="fr")
+
+    def __str__(self):
+        return self.user
+
+    @property
+    def messages_param(self):
+        return [message.as_message for message in self.messages.all()]
+
+
+class Message(models.Model):
+    class RoleOptions(models.TextChoices):
+        SYSTEM = "system", "system"
+        USER = "user", "user"
+        ASSISTANT = "assistant", "assistant"
+
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name="messages",
+    )
+    role = models.CharField(max_length=50, choices=RoleOptions.choices)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.role
+
+    @property
+    def as_message(self):
+        return {"role": self.role, "content": self.content}
+
+    # todo figure out how to store the Audio version of the message.
