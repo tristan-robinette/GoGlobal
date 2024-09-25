c7bb9cef3312da3946bb6bccde888c129dbb9173
tristan-robinette <tristan-robinette@hotmail.ca>
2024-09-25 19:56:52 +0100

    typo

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
-    default=["localhost", ".sslip.io", ".datafactory"],
+    default=["localhost", ".sslip.io", ".datafactory.run"],
 )
 
 # DATABASES
